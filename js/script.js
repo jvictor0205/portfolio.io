312087fd49df5cc871821057afc6ef1aa9f90525
joaovictor-aragao <j.victor0205@gmail.com>
2024-09-12 22:54:10 -0300

    Modifying template

--- js/script.js
+++ js/script.js
@@ -2,11 +2,19 @@ let darkmode = localStorage.getItem('darkmode')
 const themeSwitch = document.getElementById('theme-switch')
 
 const enableDarkmode = () => {
+    // const iconSwitch = document.getElementById('icon-darkmode')
+    // iconSwitch.classList.remove('fa-sun')
+    // iconSwitch.classList.add('fa-moon')
+    
     document.body.classList.add('darkmode')
     localStorage.setItem('darkmode', 'active')
 }
 
 const disableDarkmode = () => {
+    // const iconSwitch = document.getElementById('icon-darkmode')
+    // iconSwitch.classList.remove('fa-moon')
+    // iconSwitch.classList.add('fa-sun')
+
     document.body.classList.remove('darkmode')
     localStorage.setItem('darkmode', null)
 }
@@ -17,80 +25,3 @@ themeSwitch.addEventListener("click", () => {
     darkmode = localStorage.getItem('darkmode')
     darkmode !== "active" ? enableDarkmode() : disableDarkmode()
 })
-
-// Typewrite animation
-var TxtRotate = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-
-TxtRotate.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-    var that = this;
-    var delta = 300 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-    }
-
-    setTimeout(function() {
-        that.tick();
-    }, delta);
-};
-
-window.onload = function() {
-    var elements = document.getElementsByClassName('txt-rotate');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-rotate');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-        new TxtRotate(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-    document.body.appendChild(css);
-};
-
-function changeLang() {
-    var btnLng = document.getElementById("btn-lang");
-    if (btnLng === "en-us") {
-        btnLng.value = "pt-br";
-        btnLng.innerHTML = "pt-br";k
-    } else {
-        btnLng.innerHTML = "en-us";
-    }
-}
-
-// ANIMANDO A BARRA DE HABILIDADES
-jQuery(document).ready(function(){
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},3000);
-	});
-});
