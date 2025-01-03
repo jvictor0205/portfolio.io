784900f2d7f96569397d1110c1f6615d8af17581
joaovictor-aragao <j.victor0205@gmail.com>
2025-01-03 16:13:29 -0300

    Add new layout

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
-let darkmode = localStorage.getItem('darkmode')
-const themeSwitch = document.getElementById('theme-switch')
+// let darkmode = localStorage.getItem('darkmode')
+// const themeSwitch = document.getElementById('theme-switch')
 
-const enableDarkmode = () => {
-    // const iconSwitch = document.getElementById('icon-darkmode')
-    // iconSwitch.classList.remove('fa-sun')
-    // iconSwitch.classList.add('fa-moon')
+// const enableDarkmode = () => {
+//     // const iconSwitch = document.getElementById('icon-darkmode')
+//     // iconSwitch.classList.remove('fa-sun')
+//     // iconSwitch.classList.add('fa-moon')
     
-    document.body.classList.add('darkmode')
-    localStorage.setItem('darkmode', 'active')
-}
+//     document.body.classList.add('darkmode')
+//     localStorage.setItem('darkmode', 'active')
+// }
 
-const disableDarkmode = () => {
-    // const iconSwitch = document.getElementById('icon-darkmode')
-    // iconSwitch.classList.remove('fa-moon')
-    // iconSwitch.classList.add('fa-sun')
+// const disableDarkmode = () => {
+//     // const iconSwitch = document.getElementById('icon-darkmode')
+//     // iconSwitch.classList.remove('fa-moon')
+//     // iconSwitch.classList.add('fa-sun')
 
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
-}
+//     document.body.classList.remove('darkmode')
+//     localStorage.setItem('darkmode', null)
+// }
 
-if(darkmode === "active") enableDarkmode()
+// if(darkmode === "active") enableDarkmode()
 
-themeSwitch.addEventListener("click", () => {
-    darkmode = localStorage.getItem('darkmode')
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
+// themeSwitch.addEventListener("click", () => {
+//     darkmode = localStorage.getItem('darkmode')
+//     darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+// })
